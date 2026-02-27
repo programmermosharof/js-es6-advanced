645edd2f9855d0fc1583ec2443fc1c0a270d0540
programmermosharof <mosharof.dev@gmail.com>
2026-02-27 06:08:55 +0600

    primitive and non-primitive data types

---
+++ primitive.js
@@ -0,0 +1,23 @@
+
+let amarTaka = 500; 
+
+let tomarTaka = amarTaka; 
+
+
+amarTaka = 200; 
+
+console.log("Amar Taka:", amarTaka);   
+console.log("Tomar Taka:", tomarTaka); 
+
+let user1 = {
+    name: "Mosharof",
+    role: "Learner"
+};
+
+
+let user2 = user1; 
+
+user2.name = "Gemini";
+
+console.log("User 1 Name:", user1.name);  
+console.log("User 2 Name:", user2.name); 
