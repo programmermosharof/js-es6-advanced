9fc39fe300b77706261897cc473de1bc12ac5859
programmermosharof <mosharof.dev@gmail.com>
2026-02-27 07:00:36 +0600

    null vs undefined and why undefined happens

---
+++ undeifined.js
@@ -0,0 +1,15 @@
+let date;
+console.log(date); 
+
+const add = (a, b)=> {
+    console.log(a, b);
+}
+add(5);
+
+let mySkill;
+console.log(mySkill);
+let car = {brand: "Toyota", model: "Corolla"};
+console.log(car.color);
+
+let job = null;
+console.log(job);
